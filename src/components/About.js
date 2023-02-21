de832a3334bc1c0c8ccef828b6e9e9cafdac3cbe
Marie-France Han <mf212mf@gmail.com>
2023-02-20 21:08:00 -0500

    add github repo to About component

--- src/components/About.js
+++ src/components/About.js
@@ -6,6 +6,7 @@ function About(props) {
     <p > Powered by < a href="https://newscatcherapi.com"
       target="_blank"> Newscatcher</a> and < a href="https://deepl.com"
         target="_blank" > Deepl Translator</a> </p>
+    <p> Visit my <a href='https://github.com/mfhan/newsone' target='_blank'>GitHub Repo </a></p>
   </div>
   );
 }
